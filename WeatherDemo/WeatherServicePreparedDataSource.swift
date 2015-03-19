7f5285b17efc673d6de8b9844b3de9b7f59e2854
Michael Zornek <mike@clickablebliss.com>
2015-03-19 13:38:44 -0400

    Finished implementation of prepared data source.

--- WeatherDemo/WeatherServicePreparedDataSource.swift
+++ WeatherDemo/WeatherServicePreparedDataSource.swift
@@ -1,10 +1,22 @@
 import Foundation
 
-class WeatherServicePreparedDataSource : WeatherServiceDataSource {
+struct WeatherServicePreparedDataSource : WeatherServiceDataSource {
+    
+    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.7 * Double(NSEC_PER_SEC)))
+    var fetchWeatherReportResponse: WeatherServiceFetchResult?
+    
+    init(fetchWeatherReportResponse: WeatherServiceFetchResult) {
+        self.fetchWeatherReportResponse = fetchWeatherReportResponse
+    }
     
     func fetchWeatherReport(#latitude: Double, longitude: Double, completion: (result: WeatherServiceFetchResult) -> Void) {
-        let report = WeatherReport(date: NSDate(), summary: "Mostly Cloudy", temperature: 21.43, icon: WeatherReportIcon.Snow)
-        completion(result: WeatherServiceFetchResult.Success(report))
+        dispatch_after(delayTime, dispatch_get_main_queue()) { () -> Void in
+            if let response = self.fetchWeatherReportResponse {
+                completion(result: response)
+            } else {
+                completion(result: WeatherServiceFetchResult.Failure(NSError.weatherServiceNoPreparedResponse()))
+            }
+        }
     }
     
 }
